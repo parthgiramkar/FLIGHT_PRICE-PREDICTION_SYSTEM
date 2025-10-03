abe84ddbb7c3a448c417ecd2b6a2e68ab4a37022
parthgiramkar <imparthgiramkar@gmail.com>
2025-10-03 20:27:11 +0530

    updated the templates format

--- app.py
+++ app.py
@@ -15,37 +15,38 @@ model = joblib.load("model.joblib")     # expecting the path of joblib
 @opy.route("/home")
 
 def h() :
-    return render_template("home.html",title="Home")
+	return render_template("home.html",title="Home")
 
 
 # @opy.route("/predict")                # deafult in GET method
 @opy.route("/predict" , methods=["GET","POST"])
 def pred() :
 
-    fog = Inputform()
-    if fog.validate_on_submit() :
-        x_new = pd.DataFrame(dict(
-            airline=[fog.airline.data],
-            date_of_journey=[fog.date_of_journey.data.strftime("%Y-%m-%d")],
-            source =[fog.source.data],
-            destination=[fog.destination.data],
-            arrival_time=[fog.arrival_time.data.strftime("%H:%M:%S")],
-            dep_time=[fog.dep_time.data.strftime("%H:%M:%S")],
-            duration=[fog.duration.data],
+	fog = Inputform()
+	if fog.validate_on_submit() :
+		# getting the data from the form
+		x_new = pd.DataFrame(dict(
+			airline=[fog.airline.data],
+			date_of_journey=[fog.date_of_journey.data.strftime("%Y-%m-%d")],
+			source =[fog.source.data],
+			destination=[fog.destination.data],
+			arrival_time=[fog.arrival_time.data.strftime("%H:%M:%S")],
+			dep_time=[fog.dep_time.data.strftime("%H:%M:%S")],
+			duration=[fog.duration.data],
 
-            total_stops=[fog.total_stops.data],
-            additional_info=[fog.additional_info.data],
-                        
-        ))
+			total_stops=[fog.total_stops.data],
+			additional_info=[fog.additional_info.data],
+						
+		))
 
-        prediction = model.predict(x_new)[0]       # return an array
-        mess = f"The predicted price is {prediction} INR "
-    else :
-        mess = "Please provide valid input details !"
+		prediction = model.predict(x_new)[0]       # return an array
+		mess = f"The predicted price is {prediction} INR "
+	else :
+		mess = "Please provide valid input details !"
 
-    return render_template("predict.html",title="Predict " , fo = fog , output=mess)
+	return render_template("predict.html",title="Predict " , fo = fog , output=mess)
 
 
 if __name__ == "__main__" :
 
-    opy.run(debug=True)
+	opy.run(debug=True)
